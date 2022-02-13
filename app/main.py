16a9af8b5ffb9b950c48994de14d85d5a366125f
praveen1664 <praveen1664@outlook.com>
2022-02-13 22:21:12 +0530

    adding al

--- app/main.py
+++ app/main.py
@@ -1,17 +1,55 @@
+from distutils.debug import DEBUG
 import json
+import os
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from PIL import Image
+from fastapi import (
+    FastAPI,
+    Request,
+    Depends,
+    File,
+    UploadFile,
+    HTTPException)
+import io
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+from functools import lru_cache
+import uuid
 
-BASE_DIR=pathlib.Path(__file__).parent
+# from streamlit import echo
+
+
 # print(BASE_DIR)
 # print((str(BASE_DIR/"templates")))
 # Declaring the fast api app
+# DEBUG=True if it is developement
+# DEBUG=str(os.environ.get("DEBUG"))=="1" we want to be handle automatically
+
+class Settings(BaseSettings):
+    debug: bool=False
+    echo_active: bool=False
+
+    class Config:
+        env_file=".env"
+
+
+# settings=Settings()
+
+@lru_cache
+def getSettings():
+    return Settings()
+
+DEBUG=getSettings().debug
+BASE_DIR=pathlib.Path(__file__).parent
+UPLOADED_DIR=BASE_DIR / "uploads"
+UPLOADED_DIR.mkdir(exist_ok=True)
+print(BASE_DIR)
 app=FastAPI()
 template=Jinja2Templates(str(BASE_DIR/"templates"))
+
 @app.get("/",response_class=HTMLResponse)
-def home_view(request: Request):
+def home_view(request: Request,settings: Settings = Depends (getSettings)):
     return template.TemplateResponse("home.html",{"request":request})
     # print(request)
     # return "<h1>Hello Praveen</h1>"
@@ -21,3 +59,22 @@ def home_view(request: Request):
 @app.post("/")
 def home_detail_view():
     return {"Hello":"Praveen Singh"}
+
+@app.post("/img-echo/",response_class=FileResponse)
+async def imageEchoView(file:UploadFile=File(...), settings: Settings=Depends(getSettings)):
+    if not settings.echo_active:
+        raise HTTPException (detail="Invalid EndPoint", status_code=400)
+    byte_str=io.BytesIO(await file.read())
+    try:
+        img=Image.open(byte_str) # better uses Open cv
+    except:
+        raise HTTPException (detail="Invalid Image", status_code=400)
+
+    fname=pathlib.Path(file.filename)
+    fext=fname.suffix #
+    dest= UPLOADED_DIR / f"{uuid.uuid1()}{fext}"
+    # with open(str(dest),'wb') as out:
+    #     out.write(byte_str.read())
+    img.save(dest)
+    return dest
+
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,6 +1,12 @@
+from tkinter import Image
 from django.http import response
 from fastapi.testclient import TestClient
-from app.main import app
+# from isort import file
+from app.main import app, BASE_DIR, UPLOADED_DIR
+import shutil
+import time
+from PIL import Image, ImageChops
+import io
 
 
 client=TestClient(app)
@@ -16,3 +22,43 @@ def test_home_detail_view():
     assert response.status_code==200
     assert "application/json" in response.headers['content-type']
     assert response.json() == {"Hello":"Praveen Singh"}
+
+valid_image_extension=['png','jpeg','jpg','jfif']
+def testEchoUpload():
+    img_saved_path=BASE_DIR / "Images"
+    # path=list(().glob("*"))[0]
+    for path in img_saved_path.glob("*"):
+        response=client.post("/img-echo/",files={"file":open(path,'rb')})
+        # fext=str(path.suffix).replace('.','')
+        # fext=fext.replace('jfif','jpeg')
+        # if fext in valid_image_extension:
+        try:
+            img=Image.open(path)
+        except:
+            img=None
+        if img is None:
+            # assert fext in response.headers['content-type']
+            assert response.status_code==400
+        else:
+            #returning a valid image
+            assert response.status_code==200
+            r_stream=io.BytesIO(response.content)
+            echo_img=Image.open(r_stream)
+            difference=ImageChops.difference(echo_img,img).getbbox()
+            # assert difference is None
+
+        # print(f"Path is \n {path} \n ******")
+
+        # fext=fext.replace('jfif','jpeg')
+
+        # assert "application/json" in response.headers['content-type']
+        # assert response.json() == {"Hello":"Praveen Singh"}
+        # print("*********Desired heaers are*********")
+        # print(response.headers)
+        # print(path.suffix)
+        # fext=str(path.suffix).replace('jfif','jpeg')
+
+        # print(fext)
+    # time.sleep(3)
+    shutil.rmtree(UPLOADED_DIR)
+
